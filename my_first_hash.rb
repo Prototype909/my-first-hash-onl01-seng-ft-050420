c5265973c1f0b644d8572e5b3b6fa876df3b8a99
Steve Anderson <prototype909@msn.com>
2020-05-09 03:53:14 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -17,7 +17,7 @@ def retrieval
   shipping_manifest = {
                        "whale bone corsets" => 5,
                        "porcelain vases" => 2,
-                       "oil paintings" => 3
+                       "oil paintings" => 3,
                       }
 the_manifest["oil paintings"]
 end
